14394a989a99e0ebdd612d7a3505781c9a5cfae1
Nick <86270545+NArredondo89@users.noreply.github.com>
2022-01-24 16:37:58 -0800

    revised Async/await

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   async function fetchMoviesHandler() {
     const response = await fetch('https://swapi.dev/api/films');
-    const data = response.data.json;
+    const data = await response.json();
     const transformedMovies = data.results.map((movieData) => {
       return {
         id: movieData.episode_id,
